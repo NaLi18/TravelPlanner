82946a10b04926a1a63757e755ba8058ced57ee5
Na_Li <NaLi18@github.com>
2019-12-07 15:31:15 -0500

    generate component customer

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TravelPlannerService} from '../travel-planner.service'
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
-
-  constructor() { }
+  
+  articles;
+  constructor(private travelPlanner: TravelPlannerService) { }
 
   ngOnInit() {
+    this.travelPlanner.getHotel().subscribe((data) => {
+      console.log(data);
+      this.articles = data['articles'];
+    })
   }
-
 }
--- src/app/travel-planner.service.ts
+++ src/app/travel-planner.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+import { HttpClient,HttpResponse} from '@angular/common/http';
+import { Observable } from 'rxjs'
 @Injectable({
   providedIn: 'root'
 })
 export class TravelPlannerService {
-
+  //API_KEY:string ="e06f5b3da2be4501af757078cf03a985";
   constructor(private httpClient: HttpClient) { }
+
+  public getHotel(){
+    return this.httpClient.get<Config>('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e06f5b3da2be4501af757078cf03a985');
+  }
+}
+
+export interface Config {
+  heroesUrl: string;
+  textfile: string;
 }
